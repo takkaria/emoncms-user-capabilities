ee9099aed1db95e0534180416f40577ba2fe7316
Anna Sidwell <anna@takkaria.org>
2018-07-31 22:33:11 +0100

    Fix login hook

--- user_capabilities_hooks.php
+++ user_capabilities_hooks.php
@@ -1,23 +1,30 @@
 <?php
 
-function user_capabilities_on_login_hook($mysqli) {
-    $log = new EmonLogger(__FILE__);
-
+function user_capabilities_on_login($mysqli) {
     $uid = $_SESSION['userid'];
 
     $sql = "SELECT role_capabilities.capability AS capability" .
     " FROM role_capabilities INNER JOIN user_roles ON role_capabilities.roleid=user_roles.roleid" .
     " WHERE user_roles.userid=?";
 
-    $query = $this->mysqli->prepare($sql);
+    $query = $mysqli->prepare($sql);
     $query->bind_param("d", $uid);
-    $query->execute();
-    $query->bind_result($capability);
+    $r = $query->execute();
 
     $_SESSION['capability'] = [];
 
+    $query->bind_result($capability);
     while ($query->fetch()) {
-        $log->info("capability $capability for $uid")
         $_SESSION['capability'][$capability] = true;
     }
 }
+
+/*
+
+Initial SQL
+
+INSERT INTO roles VALUES (NULL, "Super-admin", "All permissions granted");
+INSERT INTO role_capabilities VALUES (1, "groups_can_create");
+INSERT INTO user_roles VALUES (1, 1);
+
+*/
