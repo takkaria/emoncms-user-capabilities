fd0984240ad5c8cb9e36accfc372a5457a926800
Anna Sidwell <anna@takkaria.org>
2018-07-31 23:33:53 +0100

    Load capability lists

--- user_capabilities.php
+++ user_capabilities.php
@@ -8,3 +8,25 @@ function user_has_capability($cap)
     return false;
   }
 }
+
+function capability_load_list() {
+    global $capabilities;
+    $capabilities = [];
+
+    $dir = scandir("Modules");
+    foreach ($dir as $leafname) {
+        // Don't look up the tree, only down
+        if ($leafname == '.' || $leafname == '..') {
+            continue;
+        }
+
+        $dirpath = "Modules/${leafname}";
+        $capfile = "Modules/${leafname}/${leafname}_capabilities.php";
+
+        if ((is_dir($dirpath) || is_link($dirpath)) && is_file($capfile)) {
+            require $capfile;
+        }
+    }
+}
+
+capability_load_list();
