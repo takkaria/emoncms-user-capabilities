8c72ad696ee5d1017436f0ec7868b17e81d8c397
Anna Sidwell <anna@takkaria.org>
2018-08-10 12:15:30 +0100

    Add capabilities menu entry

---
+++ user_capabilities_menu.php
@@ -0,0 +1,14 @@
+<?php
+
+// Make sure we have our capability check function
+require_once "Modules/user_capabilities/user_capabilities.php";
+
+if (user_has_capability('capabilities_can_edit')) {
+	$menu_dropdown_config[] = [
+		'name' => "Capabilities",
+		'icon' => 'icon-lock',
+		'path' => "user_capabilities",
+		'order' => 50
+	];
+}
+
